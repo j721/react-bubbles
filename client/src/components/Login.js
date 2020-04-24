940fb020dc3dcf64a8680b750efdba572aa508d5
j721 <juliadvu@gmail.com>
2020-04-24 10:50:53 -0500

    passed in props to Login and set up initialState outside of Login function component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+// make a post request to retrieve a token from the api
+// when you have handled the token, navigate to the BubblePage route
+
+const initialState = {
+  username: "",
+  password: "",
+  isFetching: false
+};
+const Login = (props) => {
+
+  const [login, setLogin] = useState(initialState);
+
+  
+  const handlechange = e=>{
+    e.preventDefault();
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = e=>{
+    e.preventDefault();
+    setLogin({ ...login, isFetching: true });
+    axiosWithAuth()
+      .post("/auth/login", login)
+      .then(res => {
+        localStorage.setItem("token", res.data.message);
+        props.history.push("/bubble-page");
+      })
+      .catch(err => {
+        console.log(err, "sorry, an error has occured while logging you in");
+      });
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
