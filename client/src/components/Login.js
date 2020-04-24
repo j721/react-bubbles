324506a8100bb5c2e3047b9877faf7dae9ab70b5
j721 <juliadvu@gmail.com>
2020-04-24 11:04:19 -0500

    fixed error in .post

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,7 +14,6 @@ const Login = (props) => {
   const [login, setLogin] = useState(initialState);
 
   const handleChange = (e) => {
-    e.preventDefault();
     setLogin({ ...login, [e.target.name]: e.target.value });
   };
 
@@ -22,9 +21,9 @@ const Login = (props) => {
     e.preventDefault();
     setLogin({ ...login, isFetching: true });
     axiosWithAuth()
-      .post("/auth/login", login)
+      .post("/api/login", login)
       .then((res) => {
-        localStorage.setItem("token", res.data.message);
+        localStorage.setItem("token", res.data.payload);
         props.history.push("/bubble-page");
       })
       .catch((err) => {
